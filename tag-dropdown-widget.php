ethitter/Taxonomy-Dropdown-Widget

commit 246be76cc37abcf5f039791a223329e7c0169c3e

Add property visibility

tag-dropdown-widget.php
@@ -34,7 +34,7 @@ class taxonomy_dropdown_widget_plugin {
 	/**
 	 * Class variables
 	 */
-	var $option_defaults = array(
+	protected $option_defaults = array(
 		'taxonomy' => 'post_tag',
 		'select_name' => 'Select Tag',
 		'max_name_length' => 0,

commit f1722d3eee1513a56bab8496c248cde27e826482

Introduce magic getter to correct fatal error introduced in 246be76 when visibility was added to class properties

tag-dropdown-widget.php
@@ -77,6 +77,18 @@ class taxonomy_dropdown_widget_plugin {
 	}
 
 	/**
+	 * Allow access to certain variables that were previously public
+	 *
+	 * @return mixed
+	 */
+	public function __get( $var ) {
+		if ( 'option_defaults' == $var )
+			return $this->option_defaults;
+
+		return false;
+	}
+
+	/**
 	 * Run plugin cleanup on activation
 	 * @uses this::cleanup
 	 * @hook activation