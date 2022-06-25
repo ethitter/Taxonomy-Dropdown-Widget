337a763ef1af15b889675bae5a96f810fc0de088
Erick Hitter <git-contrib@ethitter.com>
2022-06-25 11:25:07 -0700

    Fix i18n

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function( grunt ) {
 
 		addtextdomain: {
 			options: {
-				textdomain: 'taxonomy_dropdown_widget',
+				textdomain: 'tag-dropdown-widget',
 			},
 			update_all_domains: {
 				options: {
